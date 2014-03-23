bf7c256f48e38d623153ec5643de856c67c3d3a9
Benjamin Lai <ben_lai58@192-168-1-17.tpgi.com.au>
2014-03-23 16:12:07 +1100

    [Ben] - Adding Database stuff

---
+++ src/main/java/Database/Database.java
@@ -0,0 +1,46 @@
+package Database;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Database {
+    public static void main(String[] args) {
+
+        DatabaseTestDataSource databaseTestDataSource = new DatabaseTestDataSource();
+        DataSource testDataSource = databaseTestDataSource.getDataSource();
+        doQuery(testDataSource);
+    }
+
+    private static void doQuery(DataSource dataSource) {
+
+        String query = "Select FirstName, LastName from Names Where FirstName = ?";
+
+        try {
+            Connection dbConnection = dataSource.getConnection();
+            PreparedStatement preparedStatement = dbConnection.prepareStatement(query);
+            preparedStatement.setString(1, "Ben");
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+
+                String firstName = rs.getString("FIRSTNAME");
+                String lastName = rs.getString("LASTNAME");
+
+                System.out.println("First Name : " + firstName);
+                System.out.println("Last Name : " + lastName);
+
+            }
+
+            preparedStatement.close();
+            dbConnection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
