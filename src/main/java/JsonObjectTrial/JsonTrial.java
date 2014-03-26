babe916dab7d35879cb45ffaaf104d6ffd1b7786
B_Lai <ben@bencorp.com>
2014-03-26 23:16:51 +1100

    [Ben] - Json Stuff

---
+++ src/main/java/JsonObjectTrial/JsonTrial.java
@@ -0,0 +1,17 @@
+package JsonObjectTrial;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+
+public class JsonTrial {
+
+    public static void main (String[] args) {
+        String[] array = new Array();
+        JSONObject jsonObjectMain = new JSONObject();
+        System.out.println(jsonObjectMain);
+        jsonObjectMain.put("main", "main");
+        System.out.println(jsonObjectMain);
+        jsonObjectMain.put()
+    }
+}
